12b08fa6b7356048db92a96885e964aa4d4504b8
Vick <vlopez0392@gmail.com>
2020-11-02 23:57:19 +0800

    First Commit

---
+++ JS_sandbox/app.js
@@ -0,0 +1,23 @@
+// The basics: JS comments.
+// Single line comments
+
+/*
+    Multiline comments
+*/ 
+
+//Logging to console
+console.log("Hello World"); //Logging strings
+console.log(123);  // Logging a number
+console.log(true); // Logging a boolean
+var greeting = "hello"; //Declaring a variable
+console.log((greeting)) //Checking the type pf a variable
+console.log([1,2,3,4]) //Logging an array
+console.log({a:1 ,  b:2}) //Logging an object literal
+console.table({a:1 , b:2})
+
+console.error("This is some error"); //Logging an error
+console.clear();
+console.warn("This is a warning")
+console.time("Hello")
+    console.log("Hello World")
+console.timeEnd("Hello")
